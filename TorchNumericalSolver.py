cdb42ee4116dc64a7ab975bf5ba633c852ea346c
Aki Anandam <atomicspyder@protonmail.com>
2022-04-10 19:31:12 -0700

    Added files for plotting

--- TorchNumericalSolver.py
+++ TorchNumericalSolver.py
@@ -1,14 +1,10 @@
 import torch
 from constants import *
-import numpy as np
-import sys
-
-
+import time
 
 # Differential Equations Governing Three Bodies
 # w is flattened input torch tensor with position vector followed by velocity vector
 def ThreeBodyDiffEq(w, m_1, m_2, m_3):
-
     # Unpacks flattened array
     r_1 = w[:3]
     r_2 = w[3:6]
@@ -18,9 +14,9 @@ def ThreeBodyDiffEq(w, m_1, m_2, m_3):
     v_3 = w[15:18]
 
     # Torch calculated displacement vector magnitudes
-    r_12 = torch.sqrt((r_2[0]-r_1[0])**2 + (r_2[1] - r_1[1])**2 + (r_2[2] - r_1[2])**2)
-    r_13 = torch.sqrt((r_3[0]-r_1[0])**2 + (r_3[1] - r_1[1])**2 + (r_3[2] - r_1[2])**2)
-    r_23 = torch.sqrt((r_3[0]-r_2[0])**2 + (r_3[1] - r_2[1])**2 + (r_3[2] - r_2[2])**2)
+    r_12 = torch.sqrt((r_2[0] - r_1[0]) ** 2 + (r_2[1] - r_1[1]) ** 2 + (r_2[2] - r_1[2]) ** 2)
+    r_13 = torch.sqrt((r_3[0] - r_1[0]) ** 2 + (r_3[1] - r_1[1]) ** 2 + (r_3[2] - r_1[2]) ** 2)
+    r_23 = torch.sqrt((r_3[0] - r_2[0]) ** 2 + (r_3[1] - r_2[1]) ** 2 + (r_3[2] - r_2[2]) ** 2)
 
     # The derivatives of the velocities. Returns torch tensor
     dv_1bydt = K_1 * m_2 * (r_2 - r_1) / r_12 ** 3 + K_1 * m_3 * (r_3 - r_1) / r_13 ** 3
@@ -38,10 +34,10 @@ def ThreeBodyDiffEq(w, m_1, m_2, m_3):
     # In order for Scipy module to use this it must be a 1d array
     return derivatives.flatten()
 
+
 # Returns torch tensor of the solutions to the diff eq satisfied by input vector, w/ small step dt and
 # total time time_span. Takes in flat 21 dimension input torch tensor
 def torchIntegrate(input_vector, dt, time_span):
-
     # Gets values from input vector. These will be torch tensors
     r_1 = input_vector[0:3]
     r_2 = input_vector[3:6]
@@ -53,36 +49,37 @@ def torchIntegrate(input_vector, dt, time_span):
     m_2 = input_vector[19].item()
     m_3 = input_vector[20].item()
 
-    w = torch.stack([r_1.flatten(), r_2.flatten(), r_3.flatten(), v_1.flatten(), v_2.flatten(), v_3.flatten()]).flatten()
-
+    w = torch.stack(
+        [r_1.flatten(), r_2.flatten(), r_3.flatten(), v_1.flatten(), v_2.flatten(), v_3.flatten()]).flatten()
 
     # Number of times we iterate over
-    number_of_points = time_span/dt
+    number_of_points = time_span / dt
     i = 1
-    results =  torch.stack([r_1,r_2,r_3]).flatten()[None, :]
+    results = torch.stack([r_1, r_2, r_3]).flatten()[None, :]
 
-    while (i< number_of_points):
+    while (i < number_of_points):
         derivatives = ThreeBodyDiffEq(w, m_1, m_2, m_3)
 
         # Updates values of position and velocity vectors after step
-        r_1 = torch.stack([r_1[0] + derivatives[0]*dt, r_1[1] + derivatives[1]*dt,
-                             r_1[2] + derivatives[2]*dt])
+        r_1 = torch.stack([r_1[0] + derivatives[0] * dt, r_1[1] + derivatives[1] * dt,
+                           r_1[2] + derivatives[2] * dt])
         r_2 = torch.stack([r_2[0] + derivatives[3] * dt, r_2[1] + derivatives[4] * dt,
-                            r_2[2] + derivatives[5] * dt])
+                           r_2[2] + derivatives[5] * dt])
         r_3 = torch.stack([r_3[0] + derivatives[6] * dt, r_3[1] + derivatives[7] * dt,
-                            r_3[2] + derivatives[8] * dt])
+                           r_3[2] + derivatives[8] * dt])
         v_1 = torch.stack([v_1[0] + derivatives[9] * dt, v_1[1] + derivatives[10] * dt,
-                            v_1[2] + derivatives[11] * dt])
+                           v_1[2] + derivatives[11] * dt])
         v_2 = torch.stack([v_2[0] + derivatives[12] * dt, v_2[1] + derivatives[13] * dt,
-                            v_2[2] + derivatives[14] * dt])
+                           v_2[2] + derivatives[14] * dt])
         v_3 = torch.stack([v_3[0] + derivatives[15] * dt, v_3[1] + derivatives[16] * dt,
-                            v_3[2] + derivatives[17] * dt])
+                           v_3[2] + derivatives[17] * dt])
 
         # Appends new positions to results
-        results = torch.cat((results, torch.stack([r_1,r_2,r_3]).flatten()[None, :]))
-        w = torch.stack([r_1.flatten(), r_2.flatten(), r_3.flatten(), v_1.flatten(), v_2.flatten(), v_3.flatten()]).flatten()
-
-        i+=1
+        results = torch.cat((results, torch.stack([r_1, r_2, r_3]).flatten()[None, :]))
+        w = torch.stack(
+            [r_1.flatten(), r_2.flatten(), r_3.flatten(), v_1.flatten(), v_2.flatten(), v_3.flatten()]).flatten()
+        print(i)
+        i += 1
 
     return results
 
@@ -91,11 +88,20 @@ def torchIntegrate(input_vector, dt, time_span):
 def get_final_data(input_vector, dt=.001, time_span=1):
     return torchIntegrate(input_vector, dt, time_span)[-1]
 
-if __name__ == "__main__":
-    # input_vec = torch.tensor([ 2.7088e-01, -1.0769e-01, -1.5641e-07])
-    # full_vec = torch.cat([torch.tensor([1,0,0,0,0,0,0,1,0]),input_vec, torch.tensor([0,0,0,0,0,0,1,1,1])])
 
-    input_vec = torch.tensor([ 2.3050e-01, -4.0595e-02,  8.5479e-44])
+# .001 dt for 100 s
+if __name__ == "__main__":
+
+    input_vec = torch.tensor([2.3050e-01, -4.0595e-02, 8.5479e-44])
     full_vec = torch.cat(
         [torch.tensor([1, 0, 0, 0, 0, 0, 0, 1, 0]), input_vec, torch.tensor([0, 0, 0, 0, 0, 0, 1, 1, 1])])
-    print(get_final_data(full_vec))
+
+
+
+    start = time.time()
+    print(torchIntegrate(full_vec, .001, 100))
+    end = time.time()
+    print(end - start)
+    # ans = torchIntegrate(full_vec, .001, 10).numpy()
+    # np.set_printoptions(threshold=sys.maxsize)
+    # print(ans)
---
+++ plotting.py
@@ -0,0 +1,106 @@
+from TorchNumericalSolver import torchIntegrate
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import matplotlib as mpl
+import torch
+import time
+
+mpl.rcParams['animation.ffmpeg_path'] = r'D:\Aki\Python37\Lib\site-packages\ffmpeg-5.0-essentials_build\bin\ffmpeg.exe'
+
+
+# input_vec = torch.tensor([2.3050e-01, -4.0595e-02, 8.5479e-44])
+# full_vec = torch.cat(
+#     [torch.tensor([1, 0, 0, 0, 0, 0, 0, 1, 0]), input_vec, torch.tensor([0, 0, 0, 0, 0, 0, 1, 1, 1])])
+full_vec = torch.tensor([-1, 0, 0, 1, 0, 0, 0, 0, 0, 0.347111, 0.532728, 0, 0.347111, 0.532728, 0, -2*0.347111, -2*0.532728, 0, 35, 35, 35])
+start = time.time()
+
+sol = torchIntegrate(full_vec, .001, 10).numpy()
+end = time.time()
+
+calc  = end-start
+
+first = time.time()
+r1_sol = sol[:, 0:3]
+r2_sol = sol[:, 3:6]
+r3_sol = sol[:, 6:9]
+
+
+# INITIAL POSITIONS: (-1,0), (1,0), (0,0)
+#
+# INITIAL VELOCITIES: (p1,p2), (p1,p2), (-2p1,-2p2)
+#
+# p1: 0.347111
+#
+# p2: 0.532728
+
+
+
+# Create figure
+fig = plt.figure(figsize=(20, 20))  # Create 3D axes
+ax = fig.add_subplot(111, projection="3d")  # Plot the orbits
+ax.set_zlim(-3, 3)
+ax.set_title("Visualization of orbits of stars in a three body system\n", fontsize=28)
+
+particle1, = plt.plot([], [], [], color='r')
+particle2, = plt.plot([], [], [], color='g')
+particle3, = plt.plot([], [], [], color='b')
+plt.xlim(-3, 3)
+plt.ylim(-3, 3)
+
+p1, = plt.plot([], [], marker='o', color='r', label="Real first star")
+p2, = plt.plot([], [], marker='o', color='g', label="Real second star")
+p3, = plt.plot([], [], marker='o', color='b', label="Real third star")
+
+
+# model_particle1, = plt.plot([], [], [], color='r')
+# model_particle2, = plt.plot([], [], [], color='g')
+# model_particle3, = plt.plot([], [], [], color='b')
+#
+# model_p1, = plt.plot([], [], marker='s', color='r', label="Model's first star")
+# model_p2, = plt.plot([], [], marker='s', color='g', label="Model's second star")
+# model_p3, = plt.plot([], [], marker='s', color='b', label="Model's third star")
+
+ax.legend(loc="upper left", fontsize=28)
+def update(i):
+    i*=10
+    particle1.set_data(r1_sol[:i, 0], r1_sol[:i, 1])
+    particle1.set_3d_properties(r1_sol[:i, 2])
+    particle2.set_data(r2_sol[:i, 0], r2_sol[:i, 1])
+    particle2.set_3d_properties(r2_sol[:i, 2])
+    particle3.set_data(r3_sol[:i, 0], r3_sol[:i, 1])
+    particle3.set_3d_properties(r3_sol[:i, 2])
+
+    # p1.set_data(r1_sol[i:i + 1, 0], r1_sol[i:i + 1, 1])
+    # p1.set_3d_properties(r1_sol[i:i + 1, 2])
+    # p2.set_data(r2_sol[i:i + 1, 0], r2_sol[i:i + 1, 1])
+    # p2.set_3d_properties(r2_sol[i:i + 1, 2])
+    # p3.set_data(r3_sol[i:i + 1, 0], r3_sol[i:i + 1, 1])
+    # p3.set_3d_properties(r3_sol[i:i + 1, 2])
+    print(i)
+
+    # model_particle1.set_data(model_r1_sol[:i, 0], model_r1_sol[:i, 1])
+    # model_particle1.set_3d_properties(model_r1_sol[:i, 2])
+    # model_particle2.set_data(model_r2_sol[:i, 0], model_r2_sol[:i, 1])
+    # model_particle2.set_3d_properties(model_r2_sol[:i, 2])
+    # model_particle3.set_data(model_r3_sol[:i, 0], model_r3_sol[:i, 1])
+    # model_particle3.set_3d_properties(model_r3_sol[:i, 2])
+    #
+    # model_p1.set_data(model_r1_sol[i:i + 1, 0], model_r1_sol[i:i + 1, 1])
+    # model_p1.set_3d_properties(model_r1_sol[i:i + 1, 2])
+    # model_p2.set_data(model_r2_sol[i:i + 1, 0], model_r2_sol[i:i + 1, 1])
+    # model_p2.set_3d_properties(model_r2_sol[i:i + 1, 2])
+    # model_p3.set_data(model_r3_sol[i:i + 1, 0], model_r3_sol[i:i + 1, 1])
+    # model_p3.set_3d_properties(model_r3_sol[i:i + 1, 2])
+
+    return particle1, particle2, particle3, p1, p2, p3, #model_particle1, model_particle2, model_particle3, model_p1, model_p2, model_p3
+
+
+second = time.time()
+plot_time = second-first
+
+writer = animation.FFMpegWriter(fps=100)
+ani = animation.FuncAnimation(fig, update, frames=1_000, interval=25, blit=True)
+ani.save(r"D:\Aki\Pycharm\PycharmProjects\PeriodicThreeBodies\Videos\April10\a7.mp4", writer=writer)
+second = time.time()
+print(f"Calculation: {calc}")
+print(f"Plotting: {plot_time}")
