Corrected gain calculation process and gradients

TorchNumericalSolver.py
@@ -1,6 +1,6 @@
 import torch
 from constants import *
-import time
+
 
 # Differential Equations Governing Three Bodies
 # w is flattened input torch tensor with position vector followed by velocity vector
@@ -45,9 +45,9 @@ def torchIntegrate(input_vector, dt, time_span):
     v_1 = input_vector[9:12]
     v_2 = input_vector[12:15]
     v_3 = input_vector[15:18]
-    m_1 = input_vector[18].item()
-    m_2 = input_vector[19].item()
-    m_3 = input_vector[20].item()
+    m_1 = input_vector[18]
+    m_2 = input_vector[19]
+    m_3 = input_vector[20]
 
     w = torch.stack(
         [r_1.flatten(), r_2.flatten(), r_3.flatten(), v_1.flatten(), v_2.flatten(), v_3.flatten()]).flatten()
@@ -90,9 +90,9 @@ def get_full_state(input_vector, dt, time_span):
     v_1 = input_vector[9:12]
     v_2 = input_vector[12:15]
     v_3 = input_vector[15:18]
-    m_1 = input_vector[18].item()
-    m_2 = input_vector[19].item()
-    m_3 = input_vector[20].item()
+    m_1 = input_vector[18]
+    m_2 = input_vector[19]
+    m_3 = input_vector[20]
 
     w = torch.stack(
         [r_1.flatten(), r_2.flatten(), r_3.flatten(), v_1.flatten(), v_2.flatten(), v_3.flatten()]).flatten()
@@ -134,7 +134,6 @@ def get_full_state(input_vector, dt, time_span):
 
 
 
-
 # Function for getting the final position of objects after time elapse w/ dt time interval
 def get_final_data(input_vector, dt=.001, time_span=1):
     return torchIntegrate(input_vector, dt, time_span)[-1]
@@ -142,13 +141,23 @@ def get_final_data(input_vector, dt=.001, time_span=1):
 
 # .001 dt for 100 s
 if __name__ == "__main__":
+    # input_vec = torch.tensor(
+    #     [-1, 0, 0, 1, 0, 0, 0, 0, 0, 0.347111, 0.532728, 0, 0.347111, 0.532728, 0, -2 * 0.347111, -2 * 0.532728, 0, 35,
+    #      35, 35])
+    #
+    # print(get_full_state(input_vec, 0.001, 10))
 
-    input_vec = torch.tensor([2.3050e-01, -4.0595e-02, 8.5479e-44])
-    full_vec = torch.cat(
-        [torch.tensor([1, 0, 0, 0, 0, 0, 0, 1, 0]), input_vec, torch.tensor([0, 0, 0, 0, 0, 0, 1, 1, 1])])
+    t1 = torch.tensor([1,0,0], dtype=torch.float32)
+    t2 = torch.tensor([2,0,0], dtype=torch.float32)
+    mse = torch.nn.MSELoss()
+    print(mse(t1,t2))
 
-    sol = get_full_state(full_vec, .01, 10)
-    print(sol.shape)
+    # input_vec = torch.tensor([2.3050e-01, -4.0595e-02, 8.5479e-44])
+    # full_vec = torch.cat(
+    #     [torch.tensor([1, 0, 0, 0, 0, 0, 0, 1, 0]), input_vec, torch.tensor([0, 0, 0, 0, 0, 0, 1, 1, 1])])
+    #
+    # sol = get_full_state(full_vec, .01, 10)
+    # print(sol.shape)
     # ans = torchIntegrate(full_vec, .001, 10).numpy()
     # np.set_printoptions(threshold=sys.maxsize)
     # print(ans)

main.py
@@ -22,49 +22,71 @@ def to_device(data, device):
     return data.to(device, non_blocking=True)
 
 # Starting initial vector
-input_vec = torch.tensor([-1, 0, 0, 1, 0, 0, 0, 0, 0, 0.347111, 0.532728, 0, 0.347111, 0.532728, 0, -2*0.347111, -2*0.532728, 0, 35, 35, 35], requires_grad = True)
-
+input_vec = torch.tensor([-1, 0, 0, 1, 0, 0, 0, 0, 0, 0.347111, 0.532728, 0, 0.347111, 0.532728, 0, -2*0.347111, -2*0.532728, 0, 35, 35, 35], requires_grad = True).to(device)
+input_vec.retain_grad()
 
 def forward(input_vec):
     return get_full_state(input_vec, 0.001, 10)
 
 
+# Compares two states and returns a numerical value rating how far aart the two states in the three
+# body problem are to each other. Takes in two tensor states and returns tensor of value of distance rating
+# The higher the score, the less similar they are
+def compare_states(state1, state2):
+    mse = torch.nn.MSELoss()
+    return mse(state1, state2)
+
 def most_similar_state(data_set):
-    i = 0
-    max_val = -100000000
+    i = 3
+    max_val = 100000000
     index = -1
     while i < len(data_set):
-        if torch.dot(data_set[len(data_set)-1], data_set[i]).item() > max_val:
+        if 3*torch.dot(data_set[0][:9], data_set[i][:9]).item() + torch.dot(data_set[0][9:18], data_set[i][9:18]).item() < max_val:
             index = i
-            max_val = torch.dot(data_set[len(data_set)-1], data_set[i]).item()
+            max_val = 3*torch.dot(data_set[0][:9], data_set[i][:9]).item() + torch.dot(data_set[0][9:18], data_set[i][9:18]).item()
 
         i += 1
+    print(f"Idex: {index}")
     return index
 
 
 def fit(epochs, lr, input_vec):
     i = 0
     while i < epochs:
+
         data_set = forward(input_vec)
-        state = data_set[most_similar_state(data_set)]
-        gain = torch.dot(data_set[len(data_set)-1], state)
+
+        state = data_set[most_similar_state(data_set)][:18]
+
+        gain = 3*torch.dot(data_set[0][:9], state[:9]) + torch.dot(data_set[0][9:18], state[9:18])
+        print(" ")
+        print(gain)
+
         #loss.retain_grad()
         gain.backward()
-
+        print(input_vec.grad)
         # Updates input vector
-        with torch.no_grad():
-            input_vec += input_vec.grad * lr
+
+        input_vec += input_vec.grad * lr
+        print(input_vec)
         #print(input_vec.grad)
         # Zeroes gradient
         input_vec.grad.zero_()
 
         # optimizer.step()
         # optimizer.zero_grad()
-        print(i)
+        print(f"Epoch:{i}")
+        print(" ")
         i += 1
 
 
 print(input_vec)
-fit(1000, 0.00000001, input_vec)
+a=100
+b=.0001
+fit(a, b, input_vec)
+with open("mainoutput.txt", "a") as file:
+    file.write("\n")
+    file.write(f"{a}, {b}: \n")
+    file.write(str(input_vec))
 print(input_vec)
 

plotting.py
@@ -11,7 +11,12 @@ mpl.rcParams['animation.ffmpeg_path'] = r'D:\Aki\Python37\Lib\site-packages\ffmp
 # input_vec = torch.tensor([2.3050e-01, -4.0595e-02, 8.5479e-44])
 # full_vec = torch.cat(
 #     [torch.tensor([1, 0, 0, 0, 0, 0, 0, 1, 0]), input_vec, torch.tensor([0, 0, 0, 0, 0, 0, 1, 1, 1])])
-full_vec = torch.tensor([-1, 0, 0, 1, 0, 0, 0, 0, 0, 0.347111, 0.532728, 0, 0.347111, 0.532728, 0, -2*0.347111, -2*0.532728, 0, 35.7071, 35.7071, 35.7071])
+#full_vec = torch.tensor([-1, 0, 0, 1, 0, 0, 0, 0, 0, 0.347111, 0.532728, 0, 0.347111, 0.532728, 0, -2*0.347111, -2*0.532728, 0, 35.7071, 35.7071, 35.7071])
+full_vec = torch.tensor([-1.0012e+00, -1.2444e-03,  0.0000e+00,  1.0041e+00,  1.3744e-03,
+         0.0000e+00, -2.9256e-03, -1.3002e-04,  0.0000e+00,  3.4772e-01,
+         5.3544e-01,  0.0000e+00,  3.5026e-01,  5.3328e-01,  0.0000e+00,
+        -6.9798e-01, -1.0687e+00,  0.0000e+00,  3.5000e+01,  3.5000e+01,
+         3.5000e+01])
 start = time.time()
 
 sol = torchIntegrate(full_vec, .001, 10).numpy()
@@ -100,7 +105,7 @@ plot_time = second-first
 
 writer = animation.FFMpegWriter(fps=100)
 ani = animation.FuncAnimation(fig, update, frames=1_000, interval=25, blit=True)
-ani.save(r"D:\Aki\Pycharm\PycharmProjects\PeriodicThreeBodies\Videos\April15\a4.mp4", writer=writer)
+ani.save(r"D:\Aki\Pycharm\PycharmProjects\PeriodicThreeBodies\Videos\April19\a8.mp4", writer=writer)
 second = time.time()
 print(f"Calculation: {calc}")
 print(f"Plotting: {plot_time}")

toyproblem.py
@@ -72,6 +72,8 @@ def fit(epochs, lr, input_vec):
         i += 1
 
 
-print(input_vec)
-fit(1000, 0.001, input_vec)
-print(input_vec)
+# print(input_vec)
+# fit(1000, 0.001, input_vec)
+# print(input_vec)
+t = torch.tensor([1,1])
+print(str(t))